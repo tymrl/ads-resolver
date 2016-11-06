2a55f29742f373ca7e480e59551be4086e85ffef
Tim Lewandowski <tim.lewandowski@gmail.com>
2016-11-06 18:24:19 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='ads-resolver',
+    version='0.1',
+    url='http://github.com/tymrl/ads-resolver',
+    description='',
+    classifiers=[
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: Astronomy',
+        'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Libraries'
+    ],
+    install_requires=[],
+)
