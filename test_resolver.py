3fca7229a827ff2e3aed96aa940814624c781c88
Tim Lewandowski <tim.lewandowski@gmail.com>
2016-11-13 11:21:58 -0500

    Improve a test that would accidentally pass

--- test_resolver.py
+++ test_resolver.py
@@ -10,9 +10,9 @@ class ResolverTestCase(TestCase):
             self.refmap = json.loads(f.read())
 
     def test_get_year(self):
-        eq_(get_year('Something that someone wrote in 1989, page'), '1989')
+        eq_(get_year('Something that someone wrote in 1989, page #'), '1989')
         eq_(get_year('Lots 234 of 20, numbers, 2003, 12983'), '2003')
-        eq_(get_year('2210 is after the current date, 1999 is not'), '1999')
+        eq_(get_year('1999 is before the current date, 2210 is after'), '1999')
         # Not ideal behavior, but still.
         eq_(get_year('When 1978 is in doubt, take the last 2008'), '2008')
         eq_(get_year('If there is no year, we can fix it later'), None)
