0d0eb5a97f49699420c7631f204b0524d8570d68
Tim Lewandowski <tim.lewandowski@gmail.com>
2016-11-13 21:04:15 -0500

    Make the app actually use the Resolver

--- app.py
+++ app.py
@@ -2,12 +2,15 @@ import json
 
 import flask
 
+from resolver import Resolver
+
 app = flask.Flask(__name__)
+app.resolver = Resolver()
 
 @app.route('/')
 def resolve_refstring():
     refstring = flask.request.values.get('refstring')
     return flask.jsonify({
         'refstring': refstring,
-        'bibcode': '1990ApJ...357....1A'
+        'bibcode': app.resolver.make_bibcode(refstring)
     })
